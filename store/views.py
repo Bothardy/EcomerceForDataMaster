87c3571dc85a3cbfb7496e20e7a8eb9f52f7e55f
bothardy <youshii14@gmail.com>
2024-01-18 18:17:29 +0100

    empty store updated

--- store/views.py
+++ store/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+
+    # Retrieve all products from the database
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+
+def home(request):
+    # Fetch the first product from the database (you can modify this logic based on your needs)
+    product = Product.objects.first()
+
+    return render(request, 'store/home.html', {'product': product})
