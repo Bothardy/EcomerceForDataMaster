1f0b24b99d50ef45f356fb05746058254b234531
bothardy <youshii14@gmail.com>
2024-01-18 18:53:43 +0100

    empty setting  updated

--- EcomerceForDataMaster/urls.py
+++ EcomerceForDataMaster/urls.py
@@ -1,21 +1,10 @@
-"""EcomerceForDataMaster URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# EcomerceForDataMaster/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from store.views import home  # Import the view for the home page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home.html'),  # Add this line for the root path
+    path('store/', include('store.urls')),  # Include your app's URLs
 ]
